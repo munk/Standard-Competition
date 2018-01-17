9fbd5e2db8bcdb9f7c5c35b8e535484a7ccd5b4e
John Downs <=>
2018-01-17 15:32:35 -0500

    yesod, postgres, fay

---
+++ standard-competition/app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "standard-competition" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
---
+++ standard-competition/src/Import.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Import
+    ( module Import
+    ) where
+
+import Foundation                 as Import
+import Import.NoFoundation        as Import
+import Language.Haskell.TH.Syntax (Exp (ConE))
+import Yesod.Fay                  (FayFile)
+
+fayFile :: FayFile
+fayFile = fayFile' (ConE 'StaticR)
